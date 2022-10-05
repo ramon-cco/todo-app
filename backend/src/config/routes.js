9a44ae26592db706951d65aa4e11c729d4eec8c2
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2022-10-04 22:39:37 -0300

    adding cor and headers to the backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const Modules = require('pm2/lib/API/Modules/Modules')
 
-Modules.exports = function(server){
+module.exports = function(server){
     // API Routers
-    const router = express.Router()
+    const router = express.Router() 
     server.use('/api', router) 
 
     //TODO Routes
-    const todoService = requite('../api/todo/todoService.js')
+    const todoService = require('../api/todo/todoService.js')
     todoService.register(router, '/todos')
 }
